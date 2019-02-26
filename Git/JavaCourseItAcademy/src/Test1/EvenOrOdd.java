1262ce6c243295dd6bb323c7c06aed96ff4fe9e0
magdalenabartczak <magdalenabartczak87@gmail.com>
2019-02-26 19:40:02 +0100

    message

--- Git/JavaCourseItAcademy/src/Test1/EvenOrOdd.java
+++ Git/JavaCourseItAcademy/src/Test1/EvenOrOdd.java
@@ -8,17 +8,17 @@ Tip: If the number is divisible without remainder by 2 (that is, the remainder i
  */
 public class EvenOrOdd {
     public static void main(String[] args) {
-        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 88, 45, 66, 81, 23};
         int countEven = 0;
         int countOdd = 0;
 
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
-                System.out.println(array[i] + " is even");
+ //               System.out.println(array[i] + " is even");
                 countEven++;
 
             } else {
-                System.out.println(array[i] + " is odd");
+//                System.out.println(array[i] + " is odd");
                 countOdd++;
             }
         }
